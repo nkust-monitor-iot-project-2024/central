60b096b774641fd6162c49b1de3fc77d392e100c
pan93412 <pan93412@gmail.com>
2024-05-21 09:30:31 +0800

    refactor!: wip

--- models/event.go
+++ models/event.go
@@ -1,11 +1,14 @@
 package models
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/nkust-monitor-iot-project-2024/central/protos/eventpb"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type EventType string
@@ -31,6 +34,83 @@ type Event struct {
 	CreatedAt time.Time `bson:"createdAt"`
 	// DeletedAt is the timestamp when the record was deleted.
 	DeletedAt *time.Time `bson:"deletedAt,omitempty"`
+}
+
+var (
+	ErrEventDeleted = errors.New("event is deleted")
+)
+
+// EventProtoResolver is a resolver for the event proto.
+//
+// The resolver is used to resolve the picture ID to the actual picture.
+// If the resolver is nil, the picture will be nil.
+type EventProtoResolver struct {
+	InvaderPictureResolver  func(primitive.ObjectID) ([]byte, error)
+	MovementPictureResolver func(primitive.ObjectID) ([]byte, error)
+}
+
+func (e Event) ToProto(resolver EventProtoResolver) (*eventpb.Event, error) {
+	if e.DeletedAt != nil {
+		return nil, ErrEventDeleted
+	}
+
+	metadata, err := e.Metadata.ToProto()
+	if err != nil {
+		return nil, fmt.Errorf("metadata: %w", err)
+	}
+
+	switch e.Type {
+	case EventTypeInvaded:
+		if e.Payload.Invader == nil {
+			return nil, errors.New("invader is not in payload")
+		}
+
+		invaders := make([]*eventpb.Invader, len(*e.Payload.Invader))
+		for i, invader := range *e.Payload.Invader {
+			pb, err := invader.ToProto(resolver.InvaderPictureResolver)
+			if err != nil {
+				return nil, fmt.Errorf("invader[%d]: %w", i, err)
+			}
+			invaders[i] = pb
+		}
+
+		return &eventpb.Event{
+			Id:        e.ID.Hex(),
+			CreatedAt: timestamppb.New(e.CreatedAt),
+			Event: &eventpb.Event_EventInvaded{
+				EventInvaded: &eventpb.EventInvaded{
+					Metadata: metadata,
+					Invaders: invaders,
+				},
+			},
+		}, nil
+	case EventTypeMovement:
+		if e.Payload.MovementPicture == nil {
+			return nil, errors.New("movementPicture is not in payload")
+		}
+
+		var resolvedPicture []byte
+		if resolver.MovementPictureResolver != nil {
+			var err error
+			resolvedPicture, err = resolver.MovementPictureResolver(*e.Payload.MovementPicture)
+			if err != nil {
+				return nil, fmt.Errorf("resolve movement picture: %w", err)
+			}
+		}
+
+		return &eventpb.Event{
+			Id:        e.ID.Hex(),
+			CreatedAt: timestamppb.New(e.CreatedAt),
+			Event: &eventpb.Event_EventMovement{
+				EventMovement: &eventpb.EventMovement{
+					Metadata: metadata,
+					Picture:  resolvedPicture,
+				},
+			},
+		}, nil
+	default:
+		return nil, fmt.Errorf("unknown event type: %s", e.Type)
+	}
 }
 
 var EventJsonSchema = bson.M{
@@ -79,9 +159,7 @@ var EventPayloadJsonSchema = bson.M{
 		"movementPicture": bson.M{
 			"bsonType": "objectId",
 		},
-		"movementDetectedInvader": bson.M{
-			"bsonType": "object",
-		},
+		"movementDetectedInvader": EventMovementInvaderDetectedJsonSchema,
 		"invader": bson.M{
 			"bsonType": "array",
 			"items":    EventInvaderJsonSchema,
@@ -121,6 +199,13 @@ type EventMetadata struct {
 	Timestamp time.Time `bson:"timestamp"`
 }
 
+func (m EventMetadata) ToProto() (*eventpb.EventMetadata, error) {
+	return &eventpb.EventMetadata{
+		DeviceId:  m.DeviceID,
+		Timestamp: timestamppb.New(m.Timestamp),
+	}, nil
+}
+
 var EventMetadataJsonSchema = bson.M{
 	"description": "The metadata of the event",
 	"bsonType":    "object",
@@ -143,6 +228,26 @@ type EventInvader struct {
 
 	// Confidence is the confidence of the invader.
 	Confidence float64 `bson:"confidence"`
+}
+
+// ToProto converts the EventInvader to the protobuf representation.
+//
+// The resolver function is used to resolve the picture ID to the actual picture.
+// If the resolver is nil, the picture will be nil.
+func (e EventInvader) ToProto(resolver func(primitive.ObjectID) ([]byte, error)) (*eventpb.Invader, error) {
+	var picture []byte
+	if resolver != nil {
+		var err error
+		picture, err = resolver(e.PictureID)
+		if err != nil {
+			return nil, fmt.Errorf("resolve picture: %w", err)
+		}
+	}
+
+	return &eventpb.Invader{
+		Picture:    picture,
+		Confidence: float32(e.Confidence),
+	}, nil
 }
 
 var EventInvaderJsonSchema = bson.M{
