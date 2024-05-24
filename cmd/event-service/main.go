10eb12d7a13db44fa7531073251aed0c261cb121
pan93412 <pan93412@gmail.com>
2024-05-24 14:23:32 +0800

    chore(cmd/event-service): Set Logger level

--- cmd/event-service/main.go
+++ cmd/event-service/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log/slog"
+
 	"github.com/nkust-monitor-iot-project-2024/central/internal/mq"
 	"github.com/nkust-monitor-iot-project-2024/central/internal/services/event"
 	"github.com/nkust-monitor-iot-project-2024/central/internal/telemetry"
@@ -10,6 +12,7 @@ import (
 )
 
 func main() {
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 	fx.New(
 		fx.WithLogger(utils.FxWithLoggerFn),
 		utils.ConfigFxModule,
