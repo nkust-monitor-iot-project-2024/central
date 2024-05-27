97416de37561580913081d8d3d0927cce40efa9b
pan93412 <pan93412@gmail.com>
2024-05-27 20:32:29 +0800

    feat(services/event-aggregator): Support Invaded event

--- internal/services/event-aggregator/storer.go
+++ internal/services/event-aggregator/storer.go
@@ -12,6 +12,7 @@ import (
 	"github.com/nkust-monitor-iot-project-2024/central/internal/utils"
 	"github.com/nkust-monitor-iot-project-2024/central/models"
 	"github.com/nkust-monitor-iot-project-2024/central/protos/eventpb"
+	"github.com/samber/lo"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
@@ -101,6 +102,14 @@ func (s *Storer) storeSingleEvent(ctx context.Context, event *eventpb.EventMessa
 		span.SetStatus(codes.Ok, "event stored successfully")
 
 		return status
+	case *eventpb.EventMessage_InvadedInfo:
+		status = s.storeInvadedEvent(ctx, tx, eventInfo.InvadedInfo, metadata)
+
+		span.AddEvent("created event",
+			trace.WithAttributes(otelattrext.UUID("event_id", metadata.GetEventID())))
+		span.SetStatus(codes.Ok, "event stored successfully")
+
+		return status
 	default:
 		span.SetStatus(codes.Error, "event type is not implemented")
 		span.RecordError(fmt.Errorf("event type is not implemented: %T", event.GetEvent()))
@@ -109,7 +118,7 @@ func (s *Storer) storeSingleEvent(ctx context.Context, event *eventpb.EventMessa
 }
 
 // storeMovementEvent stores the movement event in the database.
-func (s *Storer) storeMovementEvent(ctx context.Context, transaction *ent.Tx, movementInfo *eventpb.MovementInfo, metadata models.Metadata) (status bool) {
+func (s *Storer) storeMovementEvent(ctx context.Context, transaction *ent.Tx, movementInfo *eventpb.MovementInfo, metadata models.Metadata) bool {
 	ctx, span := s.tracer.Start(ctx, "event-aggregator/storer/store_movement_event")
 	defer span.End()
 
@@ -150,6 +159,65 @@ func (s *Storer) storeMovementEvent(ctx context.Context, transaction *ent.Tx, mo
 		))
 
 	span.SetStatus(codes.Ok, "created movement information in database")
+
+	return true
+}
+
+// storeInvadedEvent stores the invaded event in the database.
+func (s *Storer) storeInvadedEvent(ctx context.Context, transaction *ent.Tx, invadedInfo *eventpb.InvadedInfo, metadata models.Metadata) (status bool) {
+	ctx, span := s.tracer.Start(ctx, "event-aggregator/storer/store_invaded_event")
+	defer span.End()
+
+	span.AddEvent("create event with invaded information in database")
+	eventModelBuilder := transaction.Event.Create().
+		SetID(metadata.GetEventID()).
+		SetDeviceID(metadata.GetDeviceID()).
+		SetCreatedAt(metadata.GetEmittedAt()).
+		SetType("invaded")
+
+	if parentEventID, hasParent := metadata.GetParentEventID(); hasParent {
+		eventModelBuilder = eventModelBuilder.SetParentEventID(parentEventID)
+	}
+
+	eventModel, err := eventModelBuilder.Save(ctx)
+	if err != nil {
+		span.SetStatus(codes.Error, "failed to create event with invaded information in database")
+		span.RecordError(err)
+
+		return false
+	}
+	span.AddEvent("created event with invaded information in database",
+		trace.WithAttributes(otelattrext.UUID("event_id", eventModel.ID)))
+
+	span.AddEvent("create invader information in database")
+
+	invaderBuilders := make([]*ent.InvaderCreate, 0, len(invadedInfo.GetInvaders()))
+	for _, invader := range invadedInfo.GetInvaders() {
+		invaderBuilder := transaction.Invader.Create().
+			AddEvent(eventModel).
+			SetID(uuid.Must(uuid.NewV7())).
+			SetPicture(invader.GetPicture()).
+			SetConfidence(float64(invader.GetConfidence()))
+
+		invaderBuilders = append(invaderBuilders, invaderBuilder)
+	}
+
+	createdInvaders, err := transaction.Invader.CreateBulk(invaderBuilders...).Save(ctx)
+	if err != nil {
+		span.SetStatus(codes.Error, "failed to create invader information in database")
+		span.RecordError(err)
+
+		return false
+	}
+	span.AddEvent("created invader information in database",
+		trace.WithAttributes(
+			attribute.StringSlice("invader_ids", lo.Map(createdInvaders, func(invader *ent.Invader, _ int) string {
+				return invader.ID.String()
+			})),
+			otelattrext.UUID("event_id", eventModel.ID),
+		))
+
+	span.SetStatus(codes.Ok, "created invaded information in database")
 
 	return true
 }
