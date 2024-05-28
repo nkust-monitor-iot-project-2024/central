dc4d4e22a7b07dbd9f7d833e412efec489219fb0
pan93412 <pan93412@gmail.com>
2024-05-29 01:13:53 +0800

    fix(resource): Correct namespace value

--- internal/utils/resource.go
+++ internal/utils/resource.go
@@ -23,7 +23,7 @@ func NewResource(serviceName string, serviceVersion string) (*resource.Resource,
 		resource.NewWithAttributes(semconv.SchemaURL,
 			semconv.ServiceName(serviceName),
 			semconv.ServiceVersion(serviceVersion),
-			semconv.ServiceNamespace("iot-monitor/central"),
+			semconv.ServiceNamespace("iot-monitor"),
 			semconv.ServiceInstanceID(uuid.New().String()),
 		),
 	)
