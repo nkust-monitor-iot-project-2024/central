c24085d1680f47d5e0430ffd95673c202bc6a53c
pan93412 <pan93412@gmail.com>
2024-05-31 13:09:32 +0800

    fix(utils): Disallow duplicate call of Cleanup

--- internal/utils/cleanup_stack.go
+++ internal/utils/cleanup_stack.go
@@ -3,29 +3,41 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"sync/atomic"
 )
 
 type CleanupStack struct {
-	stack []func() error
+	stack     []func() error
+	triggered *atomic.Bool
 }
 
 // NewCleanupStack creates a new cleanup stack.
 func NewCleanupStack() *CleanupStack {
+	triggered := &atomic.Bool{}
+	triggered.Store(false)
+
 	return &CleanupStack{
-		stack: nil,
+		stack:     nil,
+		triggered: triggered,
 	}
 }
 
 // Push pushes the cleanup function to the stack.
 func (cs *CleanupStack) Push(cleanup func() error) {
+	if cs.triggered.Load() {
+		return
+	}
+
 	cs.stack = append(cs.stack, cleanup)
 }
 
 // Cleanup cleans up the stack.
 func (cs *CleanupStack) Cleanup() (err error) {
-	if cs.stack == nil {
+	if cs.stack == nil || cs.triggered.Load() {
 		return
 	}
+
+	cs.triggered.Store(true)
 
 	for i := len(cs.stack) - 1; i >= 0; i-- {
 		cleanup := cs.stack[i]
--- internal/utils/cleanup_stack_test.go
+++ internal/utils/cleanup_stack_test.go
@@ -1,6 +1,7 @@
 package utils_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/nkust-monitor-iot-project-2024/central/internal/utils"
@@ -52,5 +53,104 @@ func TestCleanupStack_MultipleFunctions(t *testing.T) {
 	}
 	if !called2 {
 		t.Error("cleanup function 2 not called")
+	}
+}
+
+func TestCleanupStack_ContinueEvenError(t *testing.T) {
+	cs := utils.NewCleanupStack()
+
+	var called1, called2 bool
+	cs.Push(func() error {
+		called1 = true
+		return nil
+	})
+	cs.Push(func() error {
+		called2 = true
+		return errors.New("error")
+	})
+
+	if err := cs.Cleanup(); err == nil {
+		t.Error("CleanupStack.Cleanup() = nil; want error")
+	}
+	if !called1 {
+		t.Error("cleanup function 1 not called")
+	}
+	if !called2 {
+		t.Error("cleanup function 2 not called")
+	}
+}
+
+func TestCleanupStack_NoDuplicateTrigger(t *testing.T) {
+	cs := utils.NewCleanupStack()
+
+	var called1, called2 int
+	cs.Push(func() error {
+		called1 += 1
+		return nil
+	})
+	cs.Push(func() error {
+		called2 += 1
+		return nil
+	})
+
+	if err := cs.Cleanup(); err != nil {
+		t.Errorf("CleanupStack.Cleanup() = %v; want nil", err)
+	}
+	if err := cs.Cleanup(); err != nil {
+		t.Errorf("CleanupStack.Cleanup() = %v; want nil", err)
+	}
+
+	if called1 != 1 {
+		t.Error("cleanup function 1 more than once")
+	}
+	if called2 != 1 {
+		t.Error("cleanup function 2 more than once")
+	}
+}
+
+func TestCleanupStack_IgnoreNewFunctionAfterCleanup(t *testing.T) {
+	cs := utils.NewCleanupStack()
+
+	var called1, called2, called3 bool
+	cs.Push(func() error {
+		called1 = true
+		return nil
+	})
+
+	cs.Push(func() error {
+		called2 = true
+		return nil
+	})
+
+	if err := cs.Cleanup(); err != nil {
+		t.Errorf("CleanupStack.Cleanup() = %v; want nil", err)
+	}
+
+	cs.Push(func() error {
+		t.Error("new cleanup function called")
+		return nil
+	})
+
+	if err := cs.Cleanup(); err != nil {
+		t.Errorf("CleanupStack.Cleanup() = %v; want nil", err)
+	}
+	if !called1 {
+		t.Error("cleanup function 1 not called")
+	}
+	if !called2 {
+		t.Error("cleanup function 2 not called")
+	}
+
+	cs.Push(func() error {
+		called3 = true
+		return nil
+	})
+
+	if err := cs.Cleanup(); err != nil {
+		t.Errorf("CleanupStack.Cleanup() = %v; want nil", err)
+	}
+
+	if called3 {
+		t.Error("cleanup function 3 is called")
 	}
 }
